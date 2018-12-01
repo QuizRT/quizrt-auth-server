3b6cf694087cd86152bb790d130e377ae2b86de5
Prateek Sahay <prateeksahay96@gmail.com>
2018-12-01 11:58:26 +0530

    Token-generating

--- Models/AuthRepo.cs
+++ Models/AuthRepo.cs
@@ -117,7 +117,7 @@ namespace quizrtAuthServer.Models
                     {
                         //string ConsulIp = Environment.GetEnvironmentVariable("MACHINE_LOCAL_IPV4");
                         //string ConsulIpHost = "http://" + ConsulIp + ":8500";
-                        string ConsulIpHost = "http://172.23.238.173:8500";
+                        string ConsulIpHost = "http://172.23.238.164:8500";
                         Console.WriteLine(ConsulIpHost);
                         client.Config.Address = new Uri(ConsulIpHost);
                         var putPair = new KVPair("secretkey")
